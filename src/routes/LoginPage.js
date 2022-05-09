41cf14511bfd0706116eb32e68fee64eeb9e66a5
jaypee-0 <eyibrajohnpaul@gmail.com>
2022-05-09 11:09:02 +0100

    clear lgs

--- src/routes/LoginPage.js
+++ src/routes/LoginPage.js
@@ -3,25 +3,8 @@ import React, { useState }from "react";
 import pablo from '../assets/pablo-sign-in-1.svg';
 import logo from '../assets/icon.svg';
 import banner from '../assets/banner.svg';
-import axios from "axios";
 
 const LoginPage = () => {
-
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  const handleSubmit = async e => {
-    e.preventDefault();
-    const email = { email, password };
-      // send the username and password to the server
-    const response = await axios.post(
-    "http://blogservice.herokuapp.com/api/login",
-    email
-  );
-  setEmail(response.data)
-  localStorage.setItem('email', response.data)
-  console.log(response.data)
-  };
 
   const [passwordShow, setPasswordShow] = useState(false);
   const togglePassword = () => {
@@ -44,10 +27,10 @@ const LoginPage = () => {
             <h2 className="h2blue pb-1">Welcome!</h2>
             <p className="pb-4">Enter details to login.</p>
             <div className="w-100">
-          <form id="loginForm" action="" onSubmit={handleSubmit} className="form-text">
-            <input type="email" placeholder="Email" value={email} onChange={({ target }) => setEmail(target.value)} className="my-3 py-2 pl-2"/>
+          <form id="loginForm" action="" className="form-text">
+            <input type="email" placeholder="Email" className="my-3 py-2 pl-2"/>
             <div className="d-flex flex-row">
-              <input id="password" type={passwordShow ? "text" : "password"} placeholder="Password" value={password} onChange={({ target }) => setPassword(target.value)} className="w-100 position-relative my-2 py-2 pl-2" />
+              <input id="password" type={passwordShow ? "text" : "password"} placeholder="Password" className="w-100 position-relative my-2 py-2 pl-2" />
               <p onClick={togglePassword} className="pr-2 cursor-pointer pt-2 mt-2 position-absolute">SHOW</p>
             </div>
             <p className="py-2">FORGOT PASSWORD ?</p>
